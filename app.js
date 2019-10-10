6e419c819b7b33182501be77eda90f9d7f8b91bb
Laila Fitriana <laila@Base22s-MBP-2.columbus.rr.com>
2019-10-10 11:57:45 -0400

    Commit

---
+++ app.js
@@ -0,0 +1,40 @@
+
+window.onload = function() {
+    // 1) Render the instance of Formio
+    Formio.setBaseUrl('https://webform-designer-stg.iop.ohio.gov');  
+  //  Formio.setBaseUrl('https://gczorxncusomhpg.form.io'); 
+    // Formio.setProjectUrl('https://gczorxncusomhpg.form.io/comment');
+    Formio.setProjectUrl('https://webform-designer-stg.iop.ohio.gov/stageauthoring-odot');
+  
+
+    Formio.createForm(document.getElementById('formio'), 'https://webform-designer-stg.iop.ohio.gov/stageauthoring-odot/comment', {
+        hooks: {
+            // 2) Use hooks to insert the data for Project ID and geo-Location before submitting the data
+          beforeSubmit: (submission, next) => {
+            // Alter the submission
+            submission.data.location = 'Franklin';
+            submission.data.projectId = '00001';
+            submission.data.status = 'new';
+            // Only call next when we are ready to submit
+            this.console.log(submission.data);
+            next();
+            console.log('The form was just submitted!!!');
+          }
+        }
+      }).then(function(form) {
+          // 3) Submit the form data
+form.on('submit', (submission) => {
+    console.log('The form was just submitted!!!');
+    console.log(submission);
+    console.log(submission.data);
+        });
+        // 4) Catching errors
+form.on('error', (errors) => {
+    this.console.log('We have errors!');
+        });
+//         // 5) If a Thank You page is needed
+// // form.on('submitDone', function(completed) {
+// //     window.location = './thanks.html';
+// // })
+//     });
+})};
